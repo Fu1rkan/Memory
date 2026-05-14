23d50923210ebfae7e8edbefc1eb215b50bb69fa
Furkan Yigit <furkanyigit2000@gmail.com>
2026-05-14 22:55:49 +0200

    feat: make start screen responsive and interactive

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-import './styles/style.scss'
+import './styles/style.scss';
+
+init();
+
+function init() {
+    const startScreen = document.getElementById('start-screen');
+    const homeScreen = document.getElementById('home-screen');
+
+    if (startScreen && homeScreen) {
+        startScreen.addEventListener('click', event => {
+            const button = (event.target as HTMLElement).closest('.start-screen__play-button') as HTMLButtonElement;
+            if (button) {
+                startScreen.classList.toggle('d_none');
+                homeScreen.classList.toggle('d_none');
+            }
+        })
+    }
+}
+
+
+
+
+
+// const button = (event.target as HTMLElement).closest('.card')
